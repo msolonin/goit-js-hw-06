86a38139ff1214bf334bf6cd783f2648fe18bdb0
Mykhailo Solonin <msolonin@anydesk.com>
2024-05-19 12:54:36 +0200

    Refactor: add blanc lines and ;

--- js/task-3.js
+++ js/task-3.js
@@ -4,18 +4,23 @@ class StringBuilder {
   constructor(initialValue) {
     this.#value = initialValue;
   }
+
   getValue(){
     return this.#value;
   }
+
   padEnd(str){
-    this.#value += str
+    this.#value += str;
   }
+
   padStart(str){
-    this.#value = str + this.#value
+    this.#value = str + this.#value;
   }
+
   padBoth(str){
-    this.#value = str + this.#value + str
+    this.#value = str + this.#value + str;
   }
+
 }
 
 
